846e703b556e19351bd76c95b93e841d3157f747
Deepinder Singh Kharoud <deepinderkharouddk@gmail.com>
2025-02-16 19:56:46 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -11,10 +11,10 @@ int main(){
         case 'B':
          printf("Good\n");
          break;
-        case 'c':
+        case 'C':
          printf("Average\n");
          break;
-        case 'd':
+        case 'D':
          printf("Below Average\n");
          break;
         case 'F';
